020afed9ba8a967dc7ded037e07c982ab3b099fb
Hassan Chewai <201274422@tafe.wa.edu.au>
2026-08-06 18:20:46 +0800

    session code updated but session 4 code deleted accidentally

--- session-02/assets/js_starter/scripts_challenge_1.js
+++ session-02/assets/js_starter/scripts_challenge_1.js
@@ -3,9 +3,14 @@
 let numbers = [1, 2, 3, 4, 5];
 
 // Use the pop() method to remove the last element from the array
-
+let newNumbers = [...numbers];
+newNumbers.pop();
+console.log(newNumbers);
 // Use the unshift() method to add the last element to the beginning of the array
-
+newNumbers.unshift(5);
+console.log(newNumbers);
 // Use the slice() method to exclude the first 3 elements and return a new array
-
+const thirdNumbers = newNumbers.slice(3);
+console.log(thirdNumbers);
 // Set the value of the outputfield inner html to the new array
+document.getElementById("OutputField").textContent = thirdNumbers;
--- session-02/assets/js_starter/scripts_challenge_2.js
+++ session-02/assets/js_starter/scripts_challenge_2.js
@@ -4,9 +4,12 @@ let array1 = [1, 2, 3, 2];
 let array2 = [4, 5, 6, 4];
 
 // Use the spread operator to merge the two arrays
-
+const arrayMerged = [...array1, ...array2];
+// console.log(arrayMerged);
 // Use the Set object to remove duplicates
-
+const arraySorted = [...new Set(arrayMerged)];
+console.log(arraySorted);
 // Use array destructuring to assign the first three elements to variables
-
+const [first, second, third, fourth, fifth, sixth] = arraySorted;
 // Set the value of the output filed to be the second element of the unique array
+document.getElementById("OutputField").textContent = arraySorted;
